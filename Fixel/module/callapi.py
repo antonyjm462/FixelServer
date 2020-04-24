7ff3d1d90feaee62af9bbd4dc89f26b5a72801f3
Antony.J.Malakkaran <35623607+antonyjm462@users.noreply.github.com>
2020-04-24 09:02:16 +0530

    Module

--- Fixel/module/callapi.py
+++ Fixel/module/callapi.py
@@ -2,29 +2,17 @@ import requests
 import convert
 import re
 
-URL = 'https://us-central1-fixel-project.cloudfunctions.net/getUser';
-r = requests.get(url=);
-s = r.json();
-user = s['data'];
-x = 'https://us-central1-fixel-project.cloudfunctions.net/getData?data={"user":"' + user + '"}';
+URL = 'https://us-central1-fixel-project.cloudfunctions.net/getUser'
 
-y = requests.get(url=x);
-
-z = y.json();
-
-data = z['data'];
-
-data = data[0];
-data = data.split(',');
-
-img_data = data[1];
-
-
-img_type = data[0];
-img_data = str.encode(img_data);
-
-
-convert.conv64(img_data,img_type);
+def main():
+  request_data = requests.get(url=URL).json()
+  user = request_data['data'];
+  POST_URL = 'https://us-central1-fixel-project.cloudfunctions.net/getData?data={"user":"' + user + '"}'
+  userData = requests.get(url=POST_URL).json()
+  imgs = userData['data'][0].split(',')
+  img_data = str.encode(imgs[1])
+  img_type = data[0];
+  convert.conv64(img_data,img_type);
 
 if __name__=="main":
   main()
